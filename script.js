e88ea47feffd3a9ee3c41a81c9ab64926af8e814
Tobin Nelson <tobin.j.n@gmail.com>
2018-08-10 20:29:22 -0600

    Make new idea card

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelector('.save-btn').addEventListener('click', makeIdea);
+document.querySelector('.idea-body').addEventListener('keyup', enableBtn);
+
+function enableBtn(e) {
+  e.preventDefault();
+  var saveBtn = document.querySelector('.save-btn')
+  if (document.querySelector('.idea-body').value !== '' && document.querySelector('.idea-title').value !== '') {
+    saveBtn.disabled = false;
+  }
+}
+
+function makeIdea(e) {
+  e.preventDefault()
+  var newTitle = document.querySelector('.idea-title').value
+  var newBody = document.querySelector('.idea-body').value
+  var ideaContainer = document.querySelector('.idea-list');
+  console.log('test')
+  var content = `<article class="m-idea-card">
+      <div class="idea-header flex-row">
+        <h2 class="search-me card-title">${newTitle}</h2>
+        <button class="delete-card svg" alt="Delete"></button>
+      </div>
+      <p class="search-me idea-description">${newBody}</p>
+      <div class="quality-buttons">
+        <button class="upvote svg" role="button" aria-label="Upvote Idea"></button>
+        <button class="downvote svg"></button>
+        <p class="quality">swill</p>
+      </div>
+      <hr>
+    </article>`
+  var newIdeaCard = document.createElement('div');
+  newIdeaCard.innerHTML = content;
+  ideaContainer.append(newIdeaCard);
+
+console.log(newIdeaCard)
+
+}
