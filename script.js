d38565480796af67c24b4f0874ef3b793c70b678
Tobin Nelson <tobin.j.n@gmail.com>
2018-08-11 10:40:46 -0600

    Add object

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var ideaContainer = document.querySelector('.idea-list');
+var ideaList = document.querySelector('.idea-list');
+var ideas = [];
 
-document.querySelector('.save-btn').addEventListener('click', makeIdea);
-document.querySelector('.idea-body').addEventListener('keyup', enableBtn);
-ideaContainer.addEventListener('click', deleteCard);
+document.querySelector('.save-btn').addEventListener('submit', addIdea);
+// document.querySelector('.idea-body').addEventListener('keyup', enableBtn);
+// ideaContainer.addEventListener('click', deleteCard);
 
 function enableBtn(e) {
   e.preventDefault();
@@ -12,39 +13,57 @@ function enableBtn(e) {
   }
 }
 
-function makeIdea(e) {
+function addIdea(e) {
   e.preventDefault()
   var newTitle = document.querySelector('.idea-title').value
   var newBody = document.querySelector('.idea-body').value
-  var content = `<article class="m-idea-card">
-      <div class="idea-header flex-row">
-        <h2 class="search-me card-title">${newTitle}</h2>
-        <button class="delete-card svg" alt="Delete"></button>
-      </div> 
-      <p class="search-me idea-description">${newBody}</p>
-      <div class="quality-buttons">
-        <button class="upvote svg" role="button" aria-label="Upvote Idea"></button>
-        <button class="downvote svg"></button>
-        <p class="quality">swill</p>
-      </div>
-      <hr>
-    </article>`
-  var newIdeaCard = document.createElement('div');
-  newIdeaCard.innerHTML = content;
-  ideaContainer.append(newIdeaCard);
-  clearInputs();
+
+  var idea = {
+    title: newTitle
+    body: newBody
+    quality: "swill"
+    }
+
+  ideas.push(idea);
+  populateIdeas(ideas, ideaList);
+  this.reset;
 }
 
-function clearInputs() {
-  document.querySelector('.idea-title').value = '';
-  document.querySelector('.idea-body').value = '';
-}
 
-function deleteCard(event) {
-  if (event.target.className === "delete-card svg") {
-    event.target.parentElement.parentElement.remove();
-  }
-}
+
+
+
+
+
+//   var content = `<article class="m-idea-card">
+//       <div class="idea-header flex-row">
+//         <h2 class="search-me card-title">${newTitle}</h2>
+//         <button class="delete-card svg" alt="Delete"></button>
+//       </div> 
+//       <p class="search-me idea-description">${newBody}</p>
+//       <div class="quality-buttons">
+//         <button class="upvote svg" role="button" aria-label="Upvote Idea"></button>
+//         <button class="downvote svg"></button>
+//         <p class="quality">swill</p>
+//       </div>
+//       <hr>
+//     </article>`
+//   var newIdeaCard = document.createElement('div');
+//   newIdeaCard.innerHTML = content;
+//   ideaContainer.append(newIdeaCard);
+//   clearInputs();
+// }
+
+// function clearInputs() {
+//   document.querySelector('.idea-title').value = '';
+//   document.querySelector('.idea-body').value = '';
+// }
+
+// function deleteCard(event) {
+//   if (event.target.className === "delete-card svg") {
+//     event.target.parentElement.parentElement.remove();
+//   }
+// }
 
 
 //make an constructor object
